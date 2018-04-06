934c7da47d44b47d3c7172a4aa7aa43d3a70ef59
tharakram <tharakram.ece@gmail.com>
2018-04-05 22:51:41 -0400

    Added HTML Pages

--- WebShoppersStop/src/main/java/com/wss/config/WssController.java
+++ WebShoppersStop/src/main/java/com/wss/config/WssController.java
@@ -3,6 +3,7 @@ package com.wss.config;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WssController {
@@ -11,6 +12,21 @@ public class WssController {
 	public String showIndex(Model model) {
 		System.out.println("Inside showIndex()");
 		return "index";
+	}
+	
+	@RequestMapping(value="/hotels.html", method=RequestMethod.GET)
+	public String showHotels(Model model) {
+		return "hotels";
+	}
+	
+	@RequestMapping(value="/about.html", method=RequestMethod.GET)
+	public String showAbout(Model model) {
+		return "about";
+	}
+	
+	@RequestMapping(value="/blog.html", method=RequestMethod.GET)
+	public String showBlog(Model model) {
+		return "blog";
 	}
 
 }
