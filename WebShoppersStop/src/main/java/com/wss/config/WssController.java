ed6f48a80bbfc7c9cce0ff271d2027713eb13d74
Nikhil <nike@Nikhils-MacBook-Air.local>
2018-04-05 23:04:10 -0400

    Website tabs
    Website tabs updated in Controller

--- WebShoppersStop/src/main/java/com/wss/config/WssController.java
+++ WebShoppersStop/src/main/java/com/wss/config/WssController.java
@@ -28,5 +28,42 @@ public class WssController {
 	public String showBlog(Model model) {
 		return "blog";
 	}
+	
+	@RequestMapping(value="/p-single.html", method=RequestMethod.GET)
+	public String showPSingle(Model model) {
+		return "p-single";
+	}
+	
+	@RequestMapping(value="/products.html", method=RequestMethod.GET)
+	public String showProducts(Model model) {
+		return "products";
+	}
+
+	@RequestMapping(value="/privacy.html", method=RequestMethod.GET)
+	public String showPrivacy(Model model) {
+		return "privacy";
+	}
+	@RequestMapping(value="/signup.html", method=RequestMethod.GET)
+	public String showSignup(Model model) {
+		return "signup";
+	}
+	@RequestMapping(value="/single.html", method=RequestMethod.GET)
+	public String showSingle(Model model) {
+		return "single";
+	}
+	@RequestMapping(value="/terms.html", method=RequestMethod.GET)
+	public String showTerms(Model model) {
+		return "terms";
+	}
+
+	@RequestMapping(value="/trains.html", method=RequestMethod.GET)
+	public String showTrains(Model model) {
+		return "trains";
+	}
+
+	@RequestMapping(value="/weekend.html", method=RequestMethod.GET)
+	public String showWeekend(Model model) {
+		return "weekend";
+	}
 
 }
